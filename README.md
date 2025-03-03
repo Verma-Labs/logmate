6fe7cbaf455dee51ebc150b07d0b5eb8ba69ce76
Atul Verma <atulverma@Atuls-MacBook-Air.local>
2025-03-02 23:38:22 -0500

    Updated LogMate to v0.1.1 with init fix

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
 LogMate is a smart log management tool for Python, offering rich text logging, filtering, and export options.
 
 ## Installation
+You can install LogMate using pip:
 
 ```sh
 pip install logmate
+
+pip install git+https://github.com/Verma-Labs/logmate.git
+
 
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="logmate",
-    version="0.1.0",  # Start with version 0.1.0
+    version="0.1.1",  # Start with version 0.1.0
     author="Atul Verma",
     author_email="atul.sv@icloud.com",
     description="A smart log management tool with rich text output, filtering, and export options.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/logmate",  # Change to your GitHub repo
+    url="https://github.com/Verma-Labs/logmate", # Change to your GitHub repo
     packages=find_packages(),
     install_requires=[
         "rich",  # For rich-text logging
